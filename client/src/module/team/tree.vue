8e3624d358a9bd9c6c3a159524b315345b54ca1d
zhaozhisheng <zhaozhisheng@aiyoumi.com>
2018-01-26 16:18:29 +0800

    update

--- client/src/module/team/tree.vue
+++ client/src/module/team/tree.vue
@@ -1,146 +1,151 @@
 <template>
-    <div class="page_TM">
-        <!-- basic show part -->
-        <div class="part_left">
-            <p class="search_unit">
-                <el-input class="txt__search" placeholder="输入关键字进行过滤" v-model="filterText">
-                </el-input>
-                <span class="btn btn__search" @click="search"></span>
-            </p>
-            <el-tree highlight-current ref="teamTree" :data="teamList" :props="defaultProps" @node-click="getInfo" :filter-node-method="filterNode"></el-tree>
-            <p class="btn_tm" @click="dgTMShow = true">
-                小组管理
-            </p>
+    <div class="shadow">
+        <div class="title">
+            <h1>团队管理</h1>
         </div>
-        <div class="part_right">
-            <h3>{{currentTeam.name}}</h3>
-            <p class="btn_addmem" @click="dgMMShow = true" v-if="teamList.length">添加成员</p>
-            <el-table height="500" ref="multipleTable" :data="teamDetail" tooltip-effect="blue" style="width: 100%">
-                <el-table-column type="selection" width="55">
-                </el-table-column>
-                <el-table-column prop="nickName" label="姓名">
-                </el-table-column>
-                <el-table-column prop="eMail" label="邮箱">
-                </el-table-column>
-                <el-table-column prop="type" label="成员类别">
-                </el-table-column>
-                <el-table-column label="操作">
-                    <template slot-scope="scope">
-                        <el-button v-if="scope.row.type !== '管理员'" size="middle" @click="memOperate('admin', scope.row)">升级</el-button>
-                        <el-button v-if="scope.row.type !== '管理员'" size="middle" type="danger" @click="memOperate('del', scope.row)">删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </div>
-        <!-- floating dialog -->
-        <!-- Team Management -->
-        <div class="dialog__container" v-show="dgTMShow">
-            <div class="dialog__content dialog_tm">
-                <div class="block_btns">
-                    <span class="btns btn_add__team" :class="{'active': activeBtn === 'add'}" @click="teamEditClick('add')">Add
-                        </span>
-                    <span class="btns btn_del__team" :class="{'active': activeBtn === 'del'}" @click="teamEditClick('del')">Del</span>
-                    <span class="btns btn_edit__team" :class="{'active': activeBtn === 'edit'}" @click="teamEditClick('edit')">Edit</span>
-                </div>
-                <h3>小组管理{{tmTipTxt}}</h3>
-                <span class="btn_dg__close" @click="dgTMShow = false">X</span>
-                <div class="block_content">
-                    <section class="section_add" v-show="activeBtn === 'add'">
-                        <p class="block_input">
-                            <p class="line_input">
-                                <label for="name">所属：</label>
-                                <span> {{rootTeam.teamName}} </span>
-                                <input name="name" type="hidden" v-model="rootTeam._id">
+        <div class="page_TM">
+            <!-- basic show part -->
+            <div class="part_left">
+                <p class="search_unit">
+                    <el-input class="txt__search" placeholder="输入关键字进行过滤" v-model="filterText">
+                    </el-input>
+                    <span class="btn btn__search" @click="search"></span>
+                </p>
+                <el-tree highlight-current ref="teamTree" :data="teamList" :props="defaultProps" @node-click="getInfo" :filter-node-method="filterNode"></el-tree>
+                <p class="btn_tm" @click="dgTMShow = true">
+                    小组管理
+                </p>
+            </div>
+            <div class="part_right">
+                <h3>{{currentTeam.name}}</h3>
+                <p class="btn_addmem" @click="dgMMShow = true" v-if="teamList.length">添加成员</p>
+                <el-table height="500" ref="multipleTable" :data="teamDetail" tooltip-effect="blue" style="width: 100%">
+                    <el-table-column type="selection" width="55">
+                    </el-table-column>
+                    <el-table-column prop="nickName" label="姓名">
+                    </el-table-column>
+                    <el-table-column prop="eMail" label="邮箱">
+                    </el-table-column>
+                    <el-table-column prop="type" label="成员类别">
+                    </el-table-column>
+                    <el-table-column label="操作">
+                        <template slot-scope="scope">
+                            <el-button v-if="scope.row.type !== '管理员'" size="middle" @click="memOperate('admin', scope.row)">升级</el-button>
+                            <el-button v-if="scope.row.type !== '管理员'" size="middle" type="danger" @click="memOperate('del', scope.row)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+            <!-- floating dialog -->
+            <!-- Team Management -->
+            <div class="dialog__container" v-show="dgTMShow">
+                <div class="dialog__content dialog_tm">
+                    <div class="block_btns">
+                        <span class="btns btn_add__team" :class="{'active': activeBtn === 'add'}" @click="teamEditClick('add')">Add
+                            </span>
+                        <span class="btns btn_del__team" :class="{'active': activeBtn === 'del'}" @click="teamEditClick('del')">Del</span>
+                        <span class="btns btn_edit__team" :class="{'active': activeBtn === 'edit'}" @click="teamEditClick('edit')">Edit</span>
+                    </div>
+                    <h3>小组管理{{tmTipTxt}}</h3>
+                    <span class="btn_dg__close" @click="dgTMShow = false">X</span>
+                    <div class="block_content">
+                        <section class="section_add" v-show="activeBtn === 'add'">
+                            <p class="block_input">
+                                <p class="line_input">
+                                    <label for="name">所属：</label>
+                                    <span> {{rootTeam.teamName}} </span>
+                                    <input name="name" type="hidden" v-model="rootTeam._id">
+                                </p>
+                                <p class="line_input">
+                                    <label for="email">名称：</label>
+                                    <input name="email" type="text" v-model="teamInfo.teamName">
+                                </p>
+                                <!-- <p class="line_input">
+                                        <label for="teamId">管理员：</label>
+                                        <input name="teamId" type="text" v-model="teamInfo.administrator">
+                                    </p> -->
                             </p>
-                            <p class="line_input">
-                                <label for="email">名称：</label>
-                                <input name="email" type="text" v-model="teamInfo.teamName">
+                            <p class="btn_addmem_submit" @click="addTeamSubmit('add')">
+                                保存
                             </p>
-                            <!-- <p class="line_input">
-                                    <label for="teamId">管理员：</label>
-                                    <input name="teamId" type="text" v-model="teamInfo.administrator">
-                                </p> -->
-                        </p>
-                        <p class="btn_addmem_submit" @click="addTeamSubmit('add')">
-                            保存
-                        </p>
-                    </section>
-                    <section class="section_edit" v-show="activeBtn === 'edit'">
-                        <p class="block_input">
-                            <p class="line_input">
-                                <label for="name">team： </label>
-                                <el-select v-model="teamInfo._id" placeholder="请选择Team">
-                                    <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
-                                    </el-option>
-                                </el-select>
+                        </section>
+                        <section class="section_edit" v-show="activeBtn === 'edit'">
+                            <p class="block_input">
+                                <p class="line_input">
+                                    <label for="name">team： </label>
+                                    <el-select v-model="teamInfo._id" placeholder="请选择Team">
+                                        <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
+                                        </el-option>
+                                    </el-select>
+                                </p>
+                                <p class="line_input">
+                                    <label for="email">名称：</label>
+                                    <input name="email" type="text" v-model="teamInfo.teamName">
+                                </p>
                             </p>
-                            <p class="line_input">
-                                <label for="email">名称：</label>
-                                <input name="email" type="text" v-model="teamInfo.teamName">
+                            <p class="btn_addmem_submit" @click="addTeamSubmit('edit')">
+                                更新
                             </p>
-                        </p>
-                        <p class="btn_addmem_submit" @click="addTeamSubmit('edit')">
-                            更新
-                        </p>
-                    </section>
-                    <section class="section_delete" v-show="activeBtn === 'del'">
-                        <el-select v-model="teamInfo._id" placeholder="请选择Team">
-                            <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
-                            </el-option>
-                        </el-select>
-                        <p class="btn_addmem_submit" @click="deleteTeamSubmit">
-                            确认删除
-                        </p>
-                    </section>
+                        </section>
+                        <section class="section_delete" v-show="activeBtn === 'del'">
+                            <el-select v-model="teamInfo._id" placeholder="请选择Team">
+                                <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
+                                </el-option>
+                            </el-select>
+                            <p class="btn_addmem_submit" @click="deleteTeamSubmit">
+                                确认删除
+                            </p>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Member Management -->
-        <div class="dialog__container" v-show="dgMMShow">
-            <div class="dialog__content">
-                <h3>添加成员</h3>
-                <!-- ({{currentTeam.name}}) -->
-                <span class="btn_dg__close" @click="dgMMShow = false">X</span>
-                <div class="tab_head">
-                    <span class="tab_quick" :class="{'active': invite_quick}" @click="invite_quick = true">快速添加</span>
-                    <span class="tab_email" :class="{'active': !invite_quick}" @click="invite_quick = false">邮件邀请</span>
-                </div>
-                <div class="tab_content">
-                    <div v-if="invite_quick" class="content_quick">
-                        <p class="block_input">
-                            <p class="line_input">
-                                <label for="name">姓名：</label>
-                                <input name="name" type="text" v-model="memInfo.name">
-                            </p>
-                            <p class="line_input">
-                                <label for="email">邮箱：</label>
-                                <input name="email" type="text" v-model="memInfo.eMail">
-                            </p>
-                            <p class="line_input">
-                                <label for="teamId">所属Team：</label>
-                                <el-select v-model="memInfo.teamId" placeholder="请选择Team">
-                                    <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
-                                    </el-option>
-                                </el-select>
-                            </p>
-                            <p class="tips">*登录账号与默认密码均为邮箱地址</p>
-                        </p>
-                        <p class="btn_addmem_submit" @click="addMemberSubmit">
-                            添加成员
-                        </p>
+            <!-- Member Management -->
+            <div class="dialog__container" v-show="dgMMShow">
+                <div class="dialog__content">
+                    <h3>添加成员</h3>
+                    <!-- ({{currentTeam.name}}) -->
+                    <span class="btn_dg__close" @click="dgMMShow = false">X</span>
+                    <div class="tab_head">
+                        <span class="tab_quick" :class="{'active': invite_quick}" @click="invite_quick = true">快速添加</span>
+                        <span class="tab_email" :class="{'active': !invite_quick}" @click="invite_quick = false">邮件邀请</span>
                     </div>
-                    <div v-else class="content_email">
-                        <p class="block_input">
-                            <p class="line_input" v-for="n in emailList.length">
-                                <label for="email">邮箱：</label>
-                                <input name="email" type="text" v-model="emailList[n-1]">
-                                <span class="btn_delete" v-show="emailList.length > 1" @click="deleteEmail(n-1)"></span>
+                    <div class="tab_content">
+                        <div v-if="invite_quick" class="content_quick">
+                            <p class="block_input">
+                                <p class="line_input">
+                                    <label for="name">姓名：</label>
+                                    <input name="name" type="text" v-model="memInfo.name">
+                                </p>
+                                <p class="line_input">
+                                    <label for="email">邮箱：</label>
+                                    <input name="email" type="text" v-model="memInfo.eMail">
+                                </p>
+                                <p class="line_input">
+                                    <label for="teamId">所属Team：</label>
+                                    <el-select v-model="memInfo.teamId" placeholder="请选择Team">
+                                        <el-option v-for="item in childrenList" :key="item._id" :label="item.teamName" :value="item._id">
+                                        </el-option>
+                                    </el-select>
+                                </p>
+                                <p class="tips">*登录账号与默认密码均为邮箱地址</p>
                             </p>
-                            <p class="tip_addemail" @click="addOneEmail">添加一个邮箱</p>
-                        </p>
-                        <p class="btn_addmem_submit" @click="sendInvite">
-                            发送邀请
-                        </p>
+                            <p class="btn_addmem_submit" @click="addMemberSubmit">
+                                添加成员
+                            </p>
+                        </div>
+                        <div v-else class="content_email">
+                            <p class="block_input">
+                                <p class="line_input" v-for="n in emailList.length">
+                                    <label for="email">邮箱：</label>
+                                    <input name="email" type="text" v-model="emailList[n-1]">
+                                    <span class="btn_delete" v-show="emailList.length > 1" @click="deleteEmail(n-1)"></span>
+                                </p>
+                                <p class="tip_addemail" @click="addOneEmail">添加一个邮箱</p>
+                            </p>
+                            <p class="btn_addmem_submit" @click="sendInvite">
+                                发送邀请
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -410,9 +415,6 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-@import '../../assets/style/rem.scss';
-@import '../../assets/style/reset.scss';
-
 * {
     box-sizing: border-box;
 }
@@ -453,14 +455,19 @@ export default {
         color: #fff;
     }
 }
-
-.page_TM {
-    position: relative; // width: 1000px;
-    min-height: 400px; // margin: 100px auto;
-    margin: 0 10%;
+.shadow {
     border: 1px solid #ccc;
     background: #fff;
-    box-shadow: 0 0 15px 0 #999; // background: #f9f9f9;
+    box-shadow: 0 0 15px 0 #999;
+    margin: 0 10%;
+}
+.title {
+    padding-left: 10px;
+}
+.page_TM {
+    position: relative;
+    min-height: 400px;
+    border-top: 1px solid #ccc;
     .part_left {
         display: inline-block;
         position: absolute;
